f9e94b384e0d65c52d2c3b5d6d6b73e9d218d2bc
team-aathmavil <shibinjose991@gmail.com>
2022-05-07 21:00:08 +0530

    sw & 404 update

--- sw.js
+++ sw.js
@@ -1,7 +1,14 @@
 importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
   );
-  workbox.routing.registerRoute(
-      ({request}) => request.destination === 'image',
-      new workbox.strategies.CacheFirst ()
-  )
+  
+  // This will trigger the importScripts() for workbox.strategies and its dependencies:
+  workbox.loadModule('workbox-strategies');
+  
+  self.addEventListener('fetch', event => {
+    if (event.request.url.endsWith('.svg', '.png')) {
+      // Referencing workbox.strategies will now work as expected.
+      const cacheFirst = new workbox.strategies.CacheFirst();
+      event.respondWith(cacheFirst.handle({request: event.request}));
+    }
+  });
